af5cc41f3eb5155172830acea9174ab785d0acca
cmvalley <cmvalley@gmail.com>
2018-04-02 22:45:18 -0400

    Initial dynamic load header/footer for index.php

---
+++ footer.php
@@ -0,0 +1,54 @@
+			<footer class="site-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="widget">
+								<h3 class="widget-title">Our address</h3>
+								<p>We are located on Highway 5, 15 minutes south from exit 34 on the I-20.</p>
+								<ul class="address">
+									<li><i class="fa fa-map-marker"></i> 6969 Highway 5, Douglasville, GA 30135</li>
+									<li><i class="fa fa-phone"></i> (678) 953 8558</li>
+									<li><i class="fa fa-envelope"></i> info@heritagesda.com</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="widget">
+								<h3 class="widget-title">Service Times</h3>
+								<table>
+									<tr>
+										<td>Sabbath School</td><td>9:15am - 10:45am</td>
+									</tr>
+									<tr>
+										<td>Divine Service</td><td>11:00am - 12:15pm</td>
+									</tr>
+									<tr>
+										<td>Mid-Week Prayer Service</td><td>5:0pm - 6:00pm</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="widget">
+								<h3 class="widget-title">Contact form</h3>
+								<form action="contactus.php" method="post" class="contact-form">
+									<div class="row">
+										<div class="col-md-6"><input type="text" name="yourname" placeholder="Your name..."></div>
+										<div class="col-md-6"><input type="text" name="email" placeholder="Email..."></div>
+									</div>
+									
+									<textarea name="comments" placeholder="Your message..."></textarea>
+									<div class="g-recaptcha" data-sitekey="6LfHZE0UAAAAAO3IJy27-wXAzdmiRCI6UvxvN_HI"></div>
+
+									<br>
+									<div class="text-right"><input type="submit" value="Send message"></div>
+									
+								</form>
+							</div>
+						</div>
+					</div> <!-- .row -->
+
+					<p class="colophon">Copyright 2017 Heritage Seventh-Day Adventist Church. All right reserved</p>
+
+				</div><!-- .container -->
+			</footer> <!-- .site-footer -->
---
+++ header.php
@@ -0,0 +1,19 @@
+<header class="site-header">
+	<div class="container">
+		<a href=<?php echo($pageurl); ?> class="branding">
+			<img src="images/SDALogo.png" alt="" class="logo">
+			<h1 class="site-title">Heritage Seventh-Day Adventist Church</h1>
+		</a>
+		<div class="main-navigation">
+		<button class="menu-toggle"><i class="fa fa-bars"></i> Menu</button>
+		<ul class="menu">
+			<li class="menu-item current-menu-item"><a href=<?php echo($pageurl); ?> ><?php echo($pagename); ?><small>Come home</small></a></li>
+			<li class="menu-item"><a href="sermons.html">Sermons <small>Hear the word</small></a></li>
+			<li class="menu-item"><a href="biblestudies.html">Bible Studies <small>Study the Word</small></a></li>
+			<!--<li class="menu-item"><a href="events.html">Events <small>Join in</small></a></li>-->
+			<li class="menu-item"><a href="contactus.html">Contact <small>Reach out</small></a></li>
+		</ul>
+	</div>
+		<div class="mobile-navigation"></div>
+	</div>
+</header> <!-- .site-header -->
