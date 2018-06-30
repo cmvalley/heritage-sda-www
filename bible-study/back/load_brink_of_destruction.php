744f3197915bfda74bd68e486b4c8acfb3f8dee7
cmvalley <cmvalley@gmail.com>
2018-06-30 14:42:16 -0400

    Creating Dynmic Bible Study Pages

---
+++ bible-study/back/load_brink_of_destruction.php
@@ -0,0 +1,194 @@
+<!--PHP page variables and include head.php for HTML head, header and navigation-->
+<?php 
+    $pagename='Bible Studies';
+    $pagetitle='Back Away From The Ledge' 
+	$pageurl='bible-study/brink-of-destruction.php';
+    include('../head.php'); 
+    
+//include db connnect
+require_once '../back/db_connect_write.php';
+$html= '<ol type="1">'
+try {
+    $sql = "SELECT b_s_id FROM `bible_study` WHERE b_s_name = ?";
+    $stmt = $conn->prepare($sql); 
+    //Set Paramaters
+    $param_b_s_name = $pagetitle;
+    $stmt->execute($param_b_s_name);
+
+    // set the resulting array to associative
+    $b_s_array = $stmt->fetch(PDO::FETCH_ASSOC) 
+    $b_s_id = $b_s_array['b_s_id']; 
+    // use b_s_id to get questions
+    $sql = "SELECT b_s_q_id, b_s_q_index, b_s_question from bible_study_question where b_s_id = ?";
+    $stmt = $conn->prepare($sql); 
+    //Set Paramaters
+    $param_b_s_id = $b_s_id;
+    $stmt->execute($param_b_s_id);
+
+    //questions loop
+    while($b_s_q_array = $stmt->fetch(PDO::FETCH_ASSOC)) 
+    {
+        $b_s_q_id = $b_s_q_array['b_s_q_id'];
+        $b_s_q_index = $b_s_q_array['b_s_q_index'];
+        $b_s_question = $b_s_q_array['b_s_question'];
+        // use b_s_q_id to get key texts
+        $sql = "SELECT b_s_q_key_text from bible_study_question_key_text where b_s_q_id = ?";
+        $stmt = $conn->prepare($sql); 
+        //Set Paramaters
+        $param_b_s_q_id = $b_s_q_id;
+        $stmt->execute($param_b_s_q_id);
+        while($b_s_q_k_t_array = $stmt->fetch(PDO::FETCH_ASSOC)) 
+        {
+            $b_s_q_key_text = $b_s_q_k_t_array['b_s_q_key_text']
+            if ($key_text_str =='')
+            {
+                $key_text_str = $b_s_q_key_text;
+            }
+            else 
+            {
+                $key_text_str .= ', ' . $b_s_q_key_text;
+            }
+        }
+        // use b_s_q_id to get key sub questions
+        $sql = "SELECT b_s_s_q_id, b_s_s_q_index, b_s_sub_question from bible_study_sub_question where b_s_q_id = ?";
+        $stmt = $conn->prepare($sql); 
+        //Set Paramaters
+        $param_b_s_q_id = $b_s_q_id;
+        $stmt->execute($param_b_s_q_id);
+        while($b_s_s_q_array = $stmt->fetch(PDO::FETCH_ASSOC)) 
+        {
+            $b_s_s_q_id = $b_s_s_q_array['b_s_s_q_id'];
+            $b_s_s_q_index = $b_s_s_q_array['b_s_s_q_index'];
+            $b_s_s_question = $b_s_s_q_array['b_s_sub_question'];
+                    // use b_s_s_q_id to get key texts
+        $sql = "SELECT b_s_q_key_text from bible_study_question_key_text where b_s_s_q_id = ?";
+        $stmt = $conn->prepare($sql); 
+        //Set Paramaters
+        $param_b_s_s_q_id = $b_s_s_q_id;
+        $stmt->execute($param_b_s_q_id);
+        while($b_s_q_k_t_array = $stmt->fetch(PDO::FETCH_ASSOC)) 
+        {
+            $b_s_q_key_text = $b_s_q_k_t_array['b_s_q_key_text']
+            if ($key_text_str =='')
+            {
+                $key_text_str = $b_s_q_key_text;
+            }
+            else 
+            {
+                $key_text_str .= ', ' . $b_s_q_key_text;
+            }
+        }
+            $html .=  '<ol type="a" > '
+            '<li>' . $b_s_s_question . '<i>' . $key_text_str . '</i></li>
+            <input type="text" name="' . $b_s_q_id . '" value="">
+        <input type="text" name="' . $b_s_q_id . '"1"' . '" value="">' ;
+        }
+        $html .= '
+        <li>' . $b_s_question . '<i>' . $key_text_str . '</i></li> 
+        <input type="text" name="' . $b_s_q_id . '" value="">
+        <input type="text" name="' . $b_s_q_id . '"1"' . '" value="">';
+
+    }
+    echo $html;
+
+
+  
+    
+
+   
+
+      
+    
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+
+
+?>
+			<main class="main-content">
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row">
+							<div class="content col-md-10 col-sm-push-1">
+								<h2 class="section-title"><?php echo $pagetitle ?></h2>
+								<div class="bible-study-all">
+									<form method="post">
+
+									<ol type="1" >
+									<li>What is the destruction of man? <i>2 Thessalonians 1:8-9, Revelation 21:8</i></li> 
+									<input type="text" name="Question1" value="">
+									<input type="text" name="Question1-1" value="">
+									<li>How do we get to the brink of destruction? <i>Matthew 7:13, Proverbs 16:18, Proverbs 14:12, Romans 8:6, Galations 6:8, Philippians 3:18-19, 2 Timothy 3:2-5, Mark 3:28-29</i></li> 
+									<input type="text" name="Question2" value="">
+									<input type="text" name="Question2-1" value="">
+									<li>Can we be at the brink of destruction and not know it? <i class="bible-study-text">Matthew 27:38-39, Luke 17:28-29, Matthew 15:10-14</i></li>
+									<input type="text" name="Question3-1" value="">
+									<input type="text" name="Question3-2" value="">
+									<li>What are some lessons we can learn from Biblical examples of those that have separated themselves from God?</li>
+									
+										<ol type="a" >
+										
+											<li>Saul. <i>1 Samuel 15:10-11,13, 16:14 </i></li>
+										<input type="text" name="Question4a-1" value="">
+										<input type="text" name="Question4a-2" value="">
+										
+											<li>Hophni & Phinehas. <i>1 Samuel 2:12-17, 25 </i></li>
+										
+										<input type="text" name="Question4b-1" value="">
+										<input type="text" name="Question4b-2" value="">
+										
+											<li>Achan. <i>Joshua 6:17-19</i></li>
+										
+											<input type="text" name="Question4c-1" value="">
+											<input type="text" name="Question4c-2" value="">
+											<li>Ananias & Sapphira. <i>Acts 5:1-4,9, Isaiah 29:15, Matthew 4:6,7 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">
+											<li>Israel. <i>Exodus 17:3,7, Deuteronomy 6:16 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">	
+											<li>Religious Leaders. <i class="bible-study-text">Matthew 16:1, Matthew 12:22-24, Acts 7:54-58 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">
+										</ol>
+									<li>How can we recognize if we/others are at the brink of destruction?  <i>Matthew 24:12, Revelation 3:14-18, Luke 6:43-45, Galatians 5:19-20 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">									
+									<li>How can we return or help others return from the brink of destruction? <i>Psalm 95:8-10, Ezekiel 33:11, Isaiah 1:16-17, Hebrews 3:13  </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">									
+									<li>What are some lessons we can learn with people that returned from the brink of destruction</li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">									
+										<ol type="a">
+											<li>Solomon. <i>Ecclesiastes 1:1-2, Ecclesiastes 12:13 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">											
+											<li>David. <i>2 Samuel 12:13, 2 Samuel 24:10 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">											
+										</ol>
+										<li>How can we avoid getting to the brink of destruction? <i>Matthew 7:12, John 15:12, Matthew 22:37-40, Galatians 6:8, 1 Timothy 6:8-10, Matthew 6:19-21 , 1 Thessalonians 5:12-22, 2 Peter 2:1-3 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">										
+										<li>What is God’s response to those who return? <i>Zechariah 1:3, Ezekiel 33:14-16, Jonah 4:2, 2 Chronicles 7:14, Isaiah 1:18 </i></li>
+											<input type="text" name="Question4d-1" value="">
+											<input type="text" name="Question4d-2" value="">										
+									</ol>
+										<input type="submit" value="Submit">
+									
+								</form>
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+		<script src="../js/jquery-1.11.1.min.js"></script>
+		<script src="../js/plugins.js"></script>
+		<script src="../js/app.js"></script>
+	</body>
+	</html>
